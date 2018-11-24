8474f3ccb64fcd586593a518c1555712875bf572
cesar <cesar@cesars-MacBook-Pro.local>
2018-11-24 10:16:12 -0500

    commit one
    commit one

---
+++ db.js
@@ -0,0 +1,62 @@
+import Sequelize from 'sequelize';
+import Faker from 'faker';
+import _ from 'lodash';
+
+const Conn = new Sequelize(
+  'example',
+  'root',
+  '',
+  {
+    dialect: 'mysql',
+    host: 'localhost'
+  }
+);
+
+const Person = Conn.define('person', {
+  firstName: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  lastName: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  email: {
+    type: Sequelize.STRING,
+    validate: {
+      isEmail: true
+    }
+  }
+});
+
+const Post = Conn.define('post', {
+  title: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  content: {
+    type: Sequelize.STRING,
+    allowNull: false
+  }
+});
+
+// Relations
+Person.hasMany(Post);
+Post.belongsTo(Person);
+
+Conn.sync({ force: true }).then(()=> {
+  _.times(10, ()=> {
+    return Person.create({
+      firstName: Faker.name.firstName(),
+      lastName: Faker.name.lastName(),
+      email: Faker.internet.email()
+    }).then(person => {
+      return person.createPost({
+        title: `Sample post by ${person.firstName}`,
+        content: 'here is some content'
+      });
+    });
+  });
+});
+
+export default Conn;
